792d6ebd453f7969eae3f2b39feeb9a56d89ffbb
Carlos Pineda <52181972+tamashirotod@users.noreply.github.com>
2024-06-14 00:30:36 -0500

    fin de seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CounterModule } from './counter/counter.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeroesModule } from './heroes/hereos.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    CounterModule,
+    HeroesModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/counter/components/counter.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `<h3>
+  Counter: {{counter}}
+  </h3>
+
+
+  <button (click)="increaseBy(+1)" >+1</button>
+  <button (click)="resetCounter()">Reset</button>
+  <button (click)="increaseBy(-1)">-1</button>
+  `
+})
+
+export class CounterComponent  {
+  constructor() { }
+
+  public counter: number = 2
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+  resetCounter(): void {
+    this.counter =10;
+  }
+}
